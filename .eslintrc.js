f0541133059d1bd8db7b4c742a5efb855ce0c5c9
mimas <zlguo@cnic.cn>
2021-01-10 13:02:53 +0800

    update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
+    // "plugin:vue/vue3-recommended",  // if use this rule set, turn "vetur.format.defaultFormatter.html" to "none" in settings.json
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
@@ -13,6 +14,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/comment-directive':'off'  //get rid of the linting error at the very last line of index.html
   }
 }
---
+++ .vscode/vetur/snippets/default typescript with defineComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '$TM_FILENAME_BASE',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+    return{
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
