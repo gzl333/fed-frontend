f2a2dfd4e566feeec49c5ab9c3ff5a12968b0223
mimas <zlguo@cnic.cn>
2021-01-28 10:14:12 +0800

    add webstorm settings

---
+++ .idea/fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>this is ${COMPONENT_NAME}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '${COMPONENT_NAME}',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+    return{
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
