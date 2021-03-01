89254a22bee4044a9ac13beda1bcc95be8e75199
mimas <zlguo@cnic.cn>
2021-03-01 13:01:01 +0800

    update: 增加了保持登录状态的功能

--- src/store/user/mutations.ts
+++ src/store/user/mutations.ts
@@ -1,10 +1,13 @@
 import { MutationTree } from 'vuex'
-import { ApiJwtInterface, UserInterface } from './state'
+import { TokenInterface, UserInterface } from './state'
 import axios from 'axios'
 
+// 注意此时context是store.state.user，而不是store.state
 const mutation: MutationTree<UserInterface> = {
-  storeToken (/* this: rightType, */context:UserInterface, payload:ApiJwtInterface) {
-    // 注意此时context是store.state.user，而不是store.state
+  storeEmail (context: UserInterface, payload: string) {
+    context.email = payload
+  },
+  storeToken (/* this: rightType, */context:UserInterface, payload:TokenInterface) {
     // vuex
     context.isLogin = true
     context.token = payload
