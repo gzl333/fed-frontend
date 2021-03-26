9bb01830079dfc39a7ba32deea964b6a4a596944
mimas <zlguo@cnic.cn>
2021-03-26 10:56:29 +0800

    fix: 修复创建云主机后vpn信息获取错误的问题

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="GlobalHeader">
+    this is GlobalHeader
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GlobalHeader',
+  components: {},
+  props: {},
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.GlobalHeader {
+}
+</style>
