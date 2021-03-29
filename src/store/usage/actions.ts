eb9aa0d3f2fa99faf61b53ee7e6ba2e19646e02f
mimas <zlguo@cnic.cn>
2021-03-29 10:44:02 +0800

    feat: 修改后端api

--- src/store/usage/actions.ts
+++ src/store/usage/actions.ts
@@ -54,7 +54,7 @@ import axios, { AxiosResponse } from 'axios'
 //   console.log(normalizedData)
 // })
 
-const apiBase = 'http://gosc.cstcloud.cn/api'
+const apiBase = 'http://vms.gosc.cstcloud.cn/api'
 const codeMap = new Map<number, string>(
   [
     [0, '无法获取状态'],
