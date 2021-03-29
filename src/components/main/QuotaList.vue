c3c4319bad39e61e0b0c5b1fd480aa3405da26fb
zhangyan <yzhang@cnic.cn>
2021-03-29 11:15:54 +0800

    main 页面适配大屏和手机屏

--- src/components/main/QuotaList.vue
+++ src/components/main/QuotaList.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="QuotaList">
-    <q-card flat bordered class="my-card">
+    <!-- 适配大屏 -->
+    <div class="my-card-big gt-sm">
       <q-card-section>
         <div class="row items-center no-wrap">
           <div class="col">
-            <div class="text-h6 text-weight-bold">资源配额使用情况</div>
+            <div class="text-h6 text-weight-bold">资源配额</div>
           </div>
           <div class="col-auto" style="visibility: hidden">
             <q-input
@@ -41,7 +42,10 @@
         </div>
       </q-card-section>
       <q-separator />
-      <q-card-section>
+      <q-card-section v-if="serviceName === null"
+        >由于网络问题，无法获取资源配额</q-card-section
+      >
+      <q-card-section v-else>
         <div class="row items-center q-gutter-md q-pt-xs">
           <!-- this is top title, use servicName to classify -->
           <q-tabs
@@ -60,9 +64,7 @@
               icon="img:main/ev5-01.png"
               style="align-self: stretch; position: relative"
             >
-              <q-badge color="nord11" text-color="nord6" floating>{{
-                service.number
-              }}</q-badge>
+              <q-badge floating>{{ service.number }}</q-badge>
             </q-tab>
           </q-tabs>
         </div>
@@ -265,7 +267,255 @@
           </q-tab-panel>
         </q-tab-panels>
       </q-card-section>
-    </q-card>
+    </div>
+    <!-- 适配手机 -->
+    <div class="my-card-small lt-md">
+      <q-card-section>
+        <div class="row items-center no-wrap">
+          <div class="col">
+            <div class="text-h6 text-weight-bold">资源配额</div>
+          </div>
+
+          <div class="col-auto">
+            <q-btn
+              color="nord8"
+              label="全部在用资源"
+              dense
+              unelevated
+              flat
+              class="q-ml-md"
+              :to="{ path: '/my/usage/vm' }"
+            />
+          </div>
+        </div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section v-if="serviceName === null"
+        >由于网络问题，无法获取资源配额</q-card-section
+      >
+      <q-card-section v-else>
+        <div class="row items-center q-gutter-md q-pt-xs">
+          <!-- this is top title, use servicName to classify -->
+          <q-tabs
+            v-model="toptab"
+            narrow-indicator
+            inline-label
+            class="text-nord8"
+            align="justify"
+            v-for="service in serviceName"
+            :key="service.name"
+          >
+            <q-tab
+              :name="service.name"
+              :label="service.name"
+              :ripple="{ color: 'orange' }"
+              icon="img:main/ev5-01.png"
+              style="align-self: stretch; position: relative"
+            >
+              <q-badge floating>{{ service.number }}</q-badge>
+            </q-tab>
+          </q-tabs>
+        </div>
+        <q-tab-panels
+          v-model="toptab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+          v-for="service in serviceName"
+          :key="service.name"
+        >
+          <q-tab-panel :name="service.name">
+            <q-splitter
+              v-model="splitterModel"
+              style="height: 100%; max-height: 1000px"
+            >
+              <!-- this is left title, use servicType to classify -->
+              <template v-slot:before>
+                <div v-for="(item, index) in type" :key="index">
+                  <div v-if="service.name === item.name">
+                    <div v-for="(it, i) in item.serviceTypes" :key="i">
+                      <div class="row items-center q-gutter-md q-pt-xs">
+                        <q-tabs
+                          v-model="innertab"
+                          narrow-indicator
+                          inline-label
+                          class="text-nord8"
+                          align="justify"
+                        >
+                          <q-tab
+                            :name="i"
+                            :label="`${it.type}`"
+                            :ripple="{ color: 'nord15' }"
+                            style="align-self: stretch; position: relative"
+                          />
+                        </q-tabs>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </template>
+              <!-- this is content -->
+              <template v-slot:after>
+                <div v-for="(item, index) in type" :key="index">
+                  <div v-if="service.name === item.name">
+                    <div v-for="(it, i) in item.serviceTypes" :key="i">
+                      <q-tab-panels
+                        v-model="innertab"
+                        animated
+                        swipeable
+                        vertical
+                        transition-prev="jump-up"
+                        transition-next="jump-up"
+                      >
+                        <q-tab-panel :name="i">
+                          <div v-if="it.expirationTime != null">
+                            <span class="text-subtitle2 text-nord11"
+                              >于{{ it.expirationTime }}到期
+                            </span>
+                          </div>
+                          <div
+                            class="row items-center wrap q-gutter-sm q-mt-sm"
+                          >
+                            <div v-if="it.vCpuTotal != 0">
+                              <span class="text-weight-bold">vCPU </span>总额{{
+                                it.vCpuTotal
+                              }}，剩余{{ it.vCpuTotal - it.vCpuUsed }}，已用{{
+                                it.vCpuUsed
+                              }}
+                            </div>
+                            <q-linear-progress
+                              class="q-mt-sm q-ml-xl q-mr-xs"
+                              size="25px"
+                              :value="it.vCpuUsed / it.vCpuTotal"
+                              color="nord8"
+                              v-if="it.vCpuTotal != 0"
+                            >
+                              <div class="absolute-full flex flex-center">
+                                <q-badge
+                                  color="nord6"
+                                  text-color="secondary"
+                                  :label="`${(
+                                    (it.vCpuUsed / it.vCpuTotal) *
+                                    100
+                                  ).toFixed(2)}%`"
+                                />
+                              </div>
+                            </q-linear-progress>
+
+                            <div class="q-mt-md" v-if="it.ramTotal != 0">
+                              <span class="text-weight-bold">RAM</span> 总额{{
+                                it.ramTotal
+                              }}MB，剩余{{ it.ramTotal - it.ramUsed }}MB，已用{{
+                                it.ramUsed
+                              }}MB
+                            </div>
+                            <q-linear-progress
+                              size="25px"
+                              :value="it.ramUsed / it.ramTotal"
+                              color="nord8"
+                              class="q-mt-sm q-ml-xl q-mr-xs"
+                              v-if="it.ramTotal != 0"
+                            >
+                              <div class="absolute-full flex flex-center">
+                                <q-badge
+                                  color="nord6"
+                                  text-color="secondary"
+                                  :label="`${(
+                                    (it.ramUsed / it.ramTotal) *
+                                    100
+                                  ).toFixed(2)}%`"
+                                />
+                              </div>
+                            </q-linear-progress>
+                            <div class="q-mt-md" v-if="it.diskTotal != 0">
+                              <span class="text-weight-bold">vDISK</span> 总额{{
+                                it.diskTotal
+                              }}GB，剩余{{
+                                it.diskTotal - it.diskUsed
+                              }}GB，已用{{ it.diskUsed }}GB
+                            </div>
+                            <q-linear-progress
+                              size="25px"
+                              :value="it.diskUsed / it.diskTotal"
+                              color="nord8"
+                              class="q-mt-sm q-ml-xl q-mr-xs"
+                              v-if="it.diskTotal != 0"
+                            >
+                              <div class="absolute-full flex flex-center">
+                                <q-badge
+                                  color="nord6"
+                                  text-color="secondary"
+                                  :label="`${(
+                                    (it.diskUsed / it.diskTotal) *
+                                    100
+                                  ).toFixed(2)}%`"
+                                />
+                              </div>
+                            </q-linear-progress>
+
+                            <div class="q-mt-md" v-if="it.publicIpTotal != 0">
+                              <span class="text-weight-bold">Public-IP</span>
+                              总额{{ it.publicIpTotal }}，剩余{{
+                                it.publicIpTotal - it.publicIpUsed
+                              }}，已用{{ it.publicIpUsed }}
+                            </div>
+                            <q-linear-progress
+                              size="25px"
+                              :value="it.publicIpUsed / it.publicIpTotal"
+                              color="nord8"
+                              class="q-mt-sm q-ml-xl q-mr-xs"
+                              v-if="it.publicIpTotal != 0"
+                            >
+                              <div class="absolute-full flex flex-center">
+                                <q-badge
+                                  color="nord6"
+                                  text-color="secondary"
+                                  :label="`${(
+                                    (it.publicIpUsed / it.publicIpTotal) *
+                                    100
+                                  ).toFixed(2)}%`"
+                                />
+                              </div>
+                            </q-linear-progress>
+                            <div class="q-mt-md" v-if="it.privateIpTotal != 0">
+                              <span class="text-weight-bold">Private-IP</span>
+                              总额{{ it.privateIpTotal }}，剩余{{
+                                it.privateIpTotal - it.privateIpUsed
+                              }}
+                              ，已用{{ it.privateIpUsed }}
+                            </div>
+                            <q-linear-progress
+                              size="25px"
+                              :value="it.privateIpUsed / it.privateIpTotal"
+                              color="nord8"
+                              class="q-mt-sm q-ml-xl q-mr-xs"
+                              v-if="it.privateIpTotal != 0"
+                            >
+                              <div class="absolute-full flex flex-center">
+                                <q-badge
+                                  color="nord6"
+                                  text-color="secondary"
+                                  :label="`${(
+                                    (it.privateIpUsed / it.privateIpTotal) *
+                                    100
+                                  ).toFixed(2)}%`"
+                                />
+                              </div>
+                            </q-linear-progress>
+                          </div>
+                        </q-tab-panel>
+                      </q-tab-panels>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </q-splitter>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card-section>
+    </div>
   </div>
 </template>
 
@@ -314,13 +564,16 @@ export default defineComponent({
 <style lang="scss" scoped>
 .QuotaList {
 }
-.my-card {
+.my-card-big {
   width: 100%;
   // height: calc(100vh - 114px - 28vh);
   // width: calc(100vw - 165px - 49vw);
   overflow: hidden;
   height: 580px;
-  font-size: 1vw;
+  font-size: 0.8333333333vw;
   width: 41.666666667vw;
+}
+.my-card-small {
+  width: 100%;
 }
 </style>
--- src/components/main/VmList.vue
+++ src/components/main/VmList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="VmList">
-    <q-card flat bordered class="my-card">
+    <!-- 适配大屏 -->
+    <div class="my-card-big gt-sm">
       <q-card-section>
         <div class="row items-center no-wrap">
           <div class="col">
@@ -41,8 +42,8 @@
       </q-card-section>
       <q-separator />
 
-      <q-card-section v-if="searchContent === ''">
-        <div class="q-pl-lg q-pr-lg flex flex-center" style="font-size: 0.9em">
+      <div v-if="searchContent === ''" style="font-size: 0.9em">
+        <div class="q-py-md q-px-lg flex flex-center">
           第 {{ paginationSelected }} 页 ，共 {{ pageCount }} 页
           <q-space />
 
@@ -54,13 +55,13 @@
           >
           </q-pagination>
         </div>
-
-        <div class="row items-center wrap">
+        <div v-if="serverList === null">由于网络问题，无法获取云主机</div>
+        <div v-else class="row items-center wrap q-px-xl q-gutter-lg">
           <div v-for="(item, index) in serverList" :key="index">
-            <div class="col-4 every-card">
-              <q-card flat>
+            <div class="col">
+              <q-card flat class="every-card-big">
                 <q-card-section horizontal>
-                  <q-card-section class="col-5 flex flex-center">
+                  <q-card-section class="col flex flex-center">
                     <q-chip v-if="!item.status" label="读取中" square>
                       <q-inner-loading showing class="inner-loading">
                         <q-spinner color="nord9" />
@@ -159,8 +160,170 @@
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <!-- 适配手机 -->
+    <div class="my-card-small lt-md">
+      <q-card-section>
+        <div class="row items-center no-wrap">
+          <div class="col">
+            <div class="text-h6 text-weight-bold">云主机</div>
+          </div>
+          <div class="col-auto gt-xs">
+            <q-input
+              dense
+              standout
+              v-model="searchContent"
+              input-class="text-right"
+              class="q-ml-sm"
+              type="search"
+            >
+              <template v-slot:append>
+                <q-icon v-if="searchContent === ''" name="search" />
+                <q-icon
+                  v-else
+                  name="clear"
+                  class="cursor-pointer"
+                  @click="searchContent = ''"
+                />
+              </template>
+            </q-input>
+          </div>
+          <div class="col-auto">
+            <q-btn
+              color="nord8"
+              label="全部云主机"
+              dense
+              unelevated
+              flat
+              class="q-ml-md"
+              :to="{ path: '/my/usage/vm' }"
+            />
+          </div>
+        </div>
       </q-card-section>
-    </q-card>
+      <q-separator />
+
+      <div v-if="searchContent === ''" style="font-size: 0.9em">
+        <div class="q-py-md q-px-lg flex flex-center">
+          第 {{ paginationSelected }} 页 ，共 {{ pageCount }} 页
+          <q-space />
+
+          <q-pagination
+            v-model="paginationSelected"
+            :max="pageCount"
+            :direction-links="true"
+            color="nord8"
+          >
+          </q-pagination>
+        </div>
+        <div v-if="serverList === null">由于网络问题，无法获取云主机</div>
+        <div v-else class="row items-center wrap">
+          <div v-for="(item, index) in serverList" :key="index">
+            <div class="col">
+              <q-card flat class="every-card-small">
+                <q-card-section horizontal>
+                  <q-card-section class="col flex flex-center">
+                    <q-chip v-if="!item.status" label="读取中" square>
+                      <q-inner-loading showing class="inner-loading">
+                        <q-spinner color="nord9" />
+                      </q-inner-loading>
+                    </q-chip>
+                    <div v-if="item.status == '运行中'">
+                      <q-btn
+                        flat
+                        v-if="item.status == '运行中'"
+                        @click="gotoVNC(item.id)"
+                      >
+                        <q-icon size="3em" name="computer"></q-icon>
+                        <q-tooltip>点击进入VNC</q-tooltip></q-btn
+                      >
+                    </div>
+                    <div v-if="item.status == '已关机'">
+                      <q-icon style="font-size: 3em"
+                        ><i class="fas fa-times"
+                          ><q-tooltip>请开机以使用VNC</q-tooltip></i
+                        ></q-icon
+                      >
+                    </div>
+                    <div>
+                      {{ item.ipv4 }}
+                    </div>
+                  </q-card-section>
+
+                  <q-card-section class="text-white">
+                    <div>
+                      <q-btn
+                        v-if="
+                          item.status !== '运行中' && item.status !== '已关机'
+                        "
+                        loading
+                        color="nord8"
+                        label="1"
+                        style="font-size: 0.5em"
+                      >
+                      </q-btn>
+                      <q-btn
+                        v-if="item.status == '运行中'"
+                        color="nord8"
+                        label="关机"
+                        unelevated
+                        dense
+                        style="font-size: 0.9em"
+                        @click="
+                          vmOperation({
+                            endPoint: item.endpoint_url,
+                            id: item.id,
+                            action: 'shutdown',
+                          })
+                        "
+                      />
+
+                      <q-btn
+                        v-if="item.status == '已关机'"
+                        color="nord8"
+                        label="开机"
+                        unelevated
+                        dense
+                        style="font-size: 0.9em"
+                        @click="
+                          vmOperation({
+                            endPoint: item.endpoint_url,
+                            id: item.id,
+                            action: 'start',
+                          })
+                        "
+                      />
+                    </div>
+                    <div>
+                      <q-btn
+                        color="nord8"
+                        label="VPN"
+                        dense
+                        unelevated
+                        disable
+                        style="font-size: 0.9em"
+                      />
+                    </div>
+                    <div>
+                      <q-btn
+                        color="nord8"
+                        label="详情"
+                        dense
+                        unelevated
+                        style="font-size: 0.9em"
+                        :to="{ path: '/my/usage/vmdetail' }"
+                        @click="more(item.id)"
+                      />
+                    </div>
+                  </q-card-section>
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -241,21 +404,30 @@ export default defineComponent({
 <style lang="scss" scoped>
 .VmList {
 }
-.my-card {
+.my-card-big {
   width: 100%;
   // height: calc(100vh - 114px - 28vh);
   height: 580px;
-  font-size: 1vw;
+  // font-size: 1vw;
   width: 41.666666667vw;
 }
-.every-card {
+.every-card-big {
   width: 100%;
   // width: calc(100vw - 165px - 81vw);
   // height: calc(100vh - 114px - 74vh);
   width: 9.8958333vw;
   height: 130px;
+  font-size: 0.83333333vw;
 }
 .inner-loading {
   background-color: transparent;
+}
+
+.my-card-small {
+  width: 100%;
+}
+.every-card-small {
+  width: 100%;
+  width: 20vw;
 }
 </style>
