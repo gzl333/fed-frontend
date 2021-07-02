f575430bc38c44c5a787c78edd5a73a14f98be98
mimas <zlguo@cnic.cn>
2021-07-02 10:15:48 +0800

    feat: server getter增加排序

--- src/store/vm/getters.ts
+++ src/store/vm/getters.ts
@@ -66,9 +66,12 @@ const getters: GetterTree<VmInterface, StateInterface> = {
   },
   getServersByServiceId (state): ServerInterface[] {
     // 根据用户选择的serviceId来返回server数组
+    // 排序函数，根据云主机创建时间降序排列
+    const sortFn = (a: ServerInterface, b: ServerInterface) => new Date(b.creation_time).getTime() - new Date(a.creation_time).getTime()
+
     // 当前选择的serviceId位于state.vm.pages.vmlist.filter，利用vmlist中的watch来修改
     if (state.pages.vmList.filter === '0') {
-      return Object.values(state.tables.userServerTable.byId)
+      return Object.values(state.tables.userServerTable.byId).sort(sortFn)
     } else {
       const rows: ServerInterface[] = []
       for (const server of Object.values(state.tables.userServerTable.byId)) {
@@ -76,7 +79,7 @@ const getters: GetterTree<VmInterface, StateInterface> = {
           rows.push(server)
         }
       }
-      return rows
+      return rows.sort(sortFn)
     }
   },
   /* vmlist 使用 */
